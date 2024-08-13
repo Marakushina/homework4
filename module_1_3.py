a04c24ee1e05c99676176a2fb78bb966ec83abb0
Marakushina <marakushina970@mail.ru>
2024-08-14 00:23:06 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name="Evgenia"
+print("Name",name,type(name))
+age=27
+print("Age:",age,type(age))
+age=age+3
+print("New age:",age,type(age))
+is_student = True
+print("Is student:",is_student,type(is_student))
